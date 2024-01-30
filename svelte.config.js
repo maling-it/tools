d415e02242c7e5b21b58058c86207e8400b369c5
Memet Zx <zxce3@gnuweeb.org>
2024-01-30 18:47:10 +0700

    Auto commit for changes in svelte.config.js
    Changes in svelte.config.js:\n\ndiff --git a/svelte.config.js b/svelte.config.js
    index 348fa32..6434d4e 100644
    --- a/svelte.config.js
    +++ b/svelte.config.js
    @@ -1,13 +1,25 @@
    -import adapter from '@sveltejs/adapter-auto';
    -
    +import adapter from "@sveltejs/adapter-static";
    +import { vitePreprocess } from "@sveltejs/kit/vite";
     /** @type {import('@sveltejs/kit').Config} */
     const config = {
     	kit: {
    -		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
    -		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
    -		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
    -		adapter: adapter()
    -	}
    -};
    +		adapter: adapter(),
    +		paths: {
    +			base: "/tools"
    +		},
    +		prerender: {
    +
    +			handleHttpError: ({ path, referrer, message }) => {
    +				// Ignore deliberate link to shiny 404 page
    +				if (path === '/404' && referrer === '/tools/error') {
    +					return;
    +				}
    
    +				// Otherwise fail the build
    +				throw new Error(message);
    +			}
    +		}
    +	},
    +	preprocess: vitePreprocess()
    +};
     export default config;
    
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
-
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+		adapter: adapter(),
+		paths: {
+			base: "/tools"
+		},
+		prerender: {
+			
+			handleHttpError: ({ path, referrer, message }) => {
+				// Ignore deliberate link to shiny 404 page
+				if (path === '/404' && referrer === '/tools/error') {
+					return;
+				}
 
+				// Otherwise fail the build
+				throw new Error(message);
+			}
+		}
+	},
+	preprocess: vitePreprocess()
+};
 export default config;
