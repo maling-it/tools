5c1442b651fed08443ad49277d29526b82c7343e
Memet Zx <zxce3@gnuweeb.org>
2024-01-30 11:29:42 +0700

    Initial Commmit
    Signed-off-by: Memet Zx <zxce3@gnuweeb.org>

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	server: {
+		host: true,
+		port: 6969,
+	},
+	plugins: [sveltekit()]
+});
