9aff2b45488089bda2e8d44bcd7ac79aa59270fb
Memet Zx <zxce3@gnuweeb.org>
2024-01-30 16:19:34 +0700

    Auto commit for changes in src/routes/+page.svelte
    Changes in src/routes/+page.svelte:\n\ndiff --git a/src/routes/+page.svelte b/src/routes/+page.svelte
    index 5982b0a..ea31669 100644
    --- a/src/routes/+page.svelte
    +++ b/src/routes/+page.svelte
    @@ -1,2 +1,16 @@
    +<script>
    +    import ModalBody from "../components/ModalBody.svelte";
    +    import ModalBtn from "../components/ModalBtn.svelte";
    +</script>
    +
    +<svelte:head>
    +    <title>Home</title>
    +</svelte:head>
    +
     <h1>Welcome to SvelteKit</h1>
    -<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
    +<p>
    +    <ModalBtn ModalId="myModal" modalName="Open Modal" />
    +
    +    <ModalBody ModalId="myModal" ModalHeader="Hello WOrld">Content of the body</ModalBody>
    +    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
    +</p>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
+<script>
+    import ModalBody from "../components/ModalBody.svelte";
+    import ModalBtn from "../components/ModalBtn.svelte";
+</script>
+
+<svelte:head>
+    <title>Home</title>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>
+    <ModalBtn ModalId="myModal" modalName="Open Modal" />
+
+    <ModalBody ModalId="myModal" ModalHeader="Hello WOrld">Content of the body</ModalBody>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
